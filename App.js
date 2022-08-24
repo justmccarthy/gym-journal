ec2679da10e27181f547e5d801185aececcfc01b
justmccarthy <jmmccarthy94@yahoo.com>
2022-08-23 22:38:57 -0700

    added tabbed navigation

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/components/HomeScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function Workout() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>workout Screen</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Profile' }}/>
+        <Tab.Screen name="Workout" component={Workout} options={{ title: 'Workout' }}/>
+      </Tab.Navigator>
+      {/* <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
+      </Stack.Navigator> */}
+    </NavigationContainer>
   );
 }
 
