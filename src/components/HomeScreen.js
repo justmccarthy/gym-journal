3ae36cc131b1b22c3ec1a021ec1a6bde909019b9
justmccarthy <jmmccarthy94@yahoo.com>
2022-09-07 18:08:47 -0700

    update gitignore

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 
-const HomeScreen = ({naavigation}) => {
+const HomeScreen = ({navigation}) => {
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#393B3B', }}>
             <Text style={{ color: 'white', }}>Home Screen</Text>
